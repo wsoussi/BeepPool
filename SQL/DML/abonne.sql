296ce79f77f3fe4d1442f14bd9778e2394c97c28
Jeremie Dautheribes <je.je.laeti@gmail.com>
2017-12-07 16:35:27 +0100

    Pour labonne

--- SQL/DML/abonne.sql
+++ SQL/DML/abonne.sql
@@ -1,1 +1,18 @@
 -- proposer un trajet
+
+
+-- participer à un trajet
+INSERT INTO PARTECIPER(numT, emailCovoitureur)
+VALUES (001, "zobi@covoitueur.fr");
+
+-- donner un avis
+INSERT INTO AVIS(numT, numDonneur, numReceveur, nbEtoile, commentaire)
+VALUES (001, 333, 555, 3, "moyen/20");
+
+-- trigger sur l'avis
+CREATE TRIGGER ctrl_avis
+BEFORE INSERT ON avis
+DECLARE
+resultat avis.numT%TYPE;
+BEGIN
+  SELECT 
