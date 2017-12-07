6a8b0f9b95022be82bce6b7d2a401831834ce623
ONIZUKAGTO <wiwi.sam96@outlook.it>
2017-12-07 15:20:07 +0100

    AJOUTE' UNE PROCEDURE

--- SQL/DML/procedures.sql
+++ SQL/DML/procedures.sql
@@ -1,8 +1,9 @@
 DECLARE
 
 BEGIN
-  IF NOT isBlocked("email") THEN
+  IF NOT isBlocked('email') THEN
       INSERT INTO trajet VALUES (prix, date_dep , date_ar , adr_rdv , adr_dep , nbPlace , conducteur , vehiculeImm , villeDepX , villeDepY , villeArrX , villeArrY , numTrajetType)
-                                ("prix", "date_dep" , "date_ar" , "adr_rdv" , "adr_dep" , "nbPlace" , "conducteur" , "vehiculeImm" , "villeDepX" , "villeDepY" , "villeArrX" , "villeArrY" , numTrajetType )
+                                ('prix', 'date_dep' , 'date_ar' , 'adr_rdv' , 'adr_dep' , 'nbPlace' , 'conducteur' , 'vehiculeImm' , 'villeDepX' , 'villeDepY' , 'villeArrX' , 'villeArrY')
+  END IF
 END;
 /
