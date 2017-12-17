1f6ac15f65197baad0a01043a96d0bdda80bf8bd
jeremie-dautheribes <jeremie.dautheribes@etu.umontpellier.fr>
2017-12-17 16:42:25 +0100

    DECIMAL(3,2) pour le prix moyen, DECIMAL(4,2) pour le temps de trajet

--- SQL/DML/procedures.sql
+++ SQL/DML/procedures.sql
@@ -99,14 +99,14 @@ END//
 
 -- Procédure calcul prix au kilomètre
 CREATE PROCEDURE calcul_prix_par_km
-(IN distance DECIMAL, IN prix DECIMAL, OUT resultat DECIMAL)
+(IN distance DECIMAL, IN prix DECIMAL, OUT resultat DECIMAL(3,2))
 BEGIN
     SET resultat = ( prix/distance );
 END//
 
 -- Procédure calcul temps moyen d'un trajet
 CREATE PROCEDURE calcul_temps_par_trajet
-(IN distance DECIMAL,OUT resultat DECIMAL)
+(IN distance DECIMAL,OUT resultat DECIMAL(4,2))
 DECLARE tmp DECIMAL;
 DECLARE nbH DECIMAL;
 BEGIN
